3ae899f88c22fbc9de7af5d9bde29648e74cf910
tahani20 <46450491+tahani20@users.noreply.github.com>
2021-03-08 18:04:18 +0300

    Add files via upload

--- withoutImport.sol
+++ withoutImport.sol
@@ -1,19 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.1;
 
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+ //import "../github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-/**
- * @title Tech Insurance tor
- * @dev complete the functions
- */
- 
-contract TechInsurance {  //is ERC721("Tahani", "CryptoNinja") 
-    /** 
-     * Define two structs
-     * 
-     * 
-     */
+
+contract TechInsurance {
     struct Product {
         uint productId;
         string productName;
@@ -24,88 +15,83 @@ contract TechInsurance {  //is ERC721("Tahani", "CryptoNinja")
         bool isValid;
         uint time;
     }
+       
+    uint productCounter;
+  
+    address public insOwner;
+    constructor() public{
+        insOwner = msg.sender;
+    }
     
     mapping(uint => Product) public productIndex;
     mapping(address => mapping(uint => Client)) public client;
     
-    uint productCounter;
     
-    address payable public insOwner;
-    
-    
-    mapping(address => uint256)private _balances;
-    mapping (uint256 => address) private _owners;
-    
-    
-    //event transfer(address from, address to, uint tokenId);
-    
-    function transferFrom(address to, uint256 _Id) public{
-       require(msg.sender != to," You are the owner of this Insurance ");
-       require(ownerOf(_Id) == msg.sender," Your not the Owner ");
-       _transfer(msg.sender, to, _Id);
-
-    }
-    
-   function balanceOf(address owner)public view returns(uint256){
-         require(owner != address(0));
-         return _balances[owner];
-     }
-     
-     function ownerOf(uint256 _id)public view returns(address){
-         address owner = _owners[_id];
-          require(owner != address(0));
-          return owner;
-     }
-     
-     
-     function _transfer(address from, address to, uint256 tokenId) public {
-        require(to != address(0));
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-    }
-
-     
-     
-    constructor(address payable _insOwner) public{
-        insOwner = _insOwner;
-    }
-    
-    function addProduct(uint _productId, string memory _productName, uint _price ) public {
-        Product memory newproduct = Product(_productId, _productName, _price, true);
-        productIndex [productCounter++] = newproduct;
+   mapping(address => uint256) private _balance;
+   mapping(uint256 => address) private _owners;
+   
+   
+   function _balanceOf(address owner) public view returns(uint256){
+       require(owner !=address(0));
+       return _balance[owner];
+   }
+   
+    function _ownerOf(uint256 _id) public view returns(address){
+       address owner = _owners[_id];
+       require(owner != address(0));
+       return _owners[_id];
+   }
+   
+    function addProduct(uint _productId, string memory _productName, uint _price ) public  {
+        Product memory newProduct = Product (_productId, _productName, _price, true);
+        productIndex[productCounter++] = newProduct;
         productCounter++;
+        _mint(msg.sender, productCounter);
     }
     
-    function changeFalse(uint _Index) public {
-     productIndex[_Index].offered = false;
+    function changeFalse(uint _Index) public  onlyOwner {
+        productIndex[_Index].offered = false;
     }
     
-    function changeTrue(uint _Index) public {
+    function changeTrue(uint _Index) public  onlyOwner{
         productIndex[_Index].offered = true;
     }
     
-    modifier change_Price(){
-        require(insOwner == msg.sender, "you are not the owner");
-        _;
+    modifier onlyOwner {
+      require(msg.sender == insOwner, "you are not the owner" );
+      _;
+      revert("do not run this function again it cost you some fees");
+     }
+     
+    function changePrice(uint _productIndex, uint _price) public  onlyOwner{ // inherit modifier
+        productIndex[_productIndex].price = _price;
     }
-
-    function changePrice(uint _productIndex, uint _price) public change_Price { //inherit modifier 
-       productIndex[_productIndex].price = _price;
-    }
-        
-    uint256 public start = block.timestamp ;
     
-    function buyInsurance( uint _Index, uint dayAfter) public payable {
-        require(productIndex[_Index].offered == true, "InsuranceId already exists");
-        require(block.timestamp < start + dayAfter * 3 days,"not allowed");
-        Client memory newClient;
-        newClient.isValid = true;
-        newClient.time = block.timestamp;
-        uint256 price = productIndex[_Index].price;
+    function buyInsurance(uint _productIndex) public  payable {
+        require(productIndex[_productIndex].offered == true, "Tha Insurance is not available");
+        Client memory newClient = Client (true, block.timestamp);
+        client[msg.sender][_productIndex] =   newClient ;
+        require(msg.value  == productIndex[_productIndex].price, "check the amount of the Insurance");
+        uint256 price = productIndex[_productIndex].price;
         payable(msg.sender).transfer(price);
     } 
     
+   function transferInsurance( address to, uint256 _id) public {
+       require(msg.sender != to," You are the owner of this Insurance ");
+       require(_ownerOf(_id) == msg.sender," Your not the Owner ");
+        _transfer(msg.sender, to, _id);
+    }
     
+    function _transfer(address from, address to, uint256 tokenId) public {
+        require(to != address(0));
+        _balance[from] -= 1;
+        _balance[to] += 1;
+        _owners[tokenId] = to;
+    }
+    
+    function _mint(address to, uint256 tokenId) public {
+        require(to != address(0));
+        _balance[to] += 1;
+        _owners[tokenId] = to;
+    }
 }
